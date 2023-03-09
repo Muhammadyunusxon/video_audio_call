7b078f085cd105a7b4f720c4c802b817bb632090
Muhammadyunusxon <muhammadyunusxonabdumannopov9@gmail.com>
2023-03-09 06:25:43 +0500

    add Call

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+
+import 'dragable_example.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        title: 'Flutter Demo', home: DraggableScrollBarDemo());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final AgoraClient client = AgoraClient(
+    agoraConnectionData: AgoraConnectionData(
+      appId: "457e3f501bba48279f5c31ac8a5b5f67",
+      channelName: "test",
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    initAgora();
+  }
+
+  void initAgora() async {
+    await client.initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            AgoraVideoViewer(client: client),
+            AgoraVideoButtons(client: client),
+          ],
+        ),
+      ),
+    );
+  }
+}
